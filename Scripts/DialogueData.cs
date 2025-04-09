3e8d0763822ac420c16de781b51eb32b5d5793af
DreamyMrL <mturg0511@gmail.com>
2025-04-08 23:36:51 -0400

    Add files via upload

---
+++ Scripts/DialogueData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewDialogue", menuName = "Dialogue/Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    public LineData[] lines;
+}
+
+
